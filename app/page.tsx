a1c2866d0991d4e31d979644fd7a959f2f364180
Andrew Tsegaye <andrewtsegaye7@gmail.com>
2024-08-26 22:28:36 +0300

    setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl underline text-white">Home</h1>
+      <Button>Click</Button>
+    </div>
+  );
+}
